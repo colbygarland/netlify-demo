163802b48610b92a604eaf4fc76e3129231fa208
Colby Garland <colbygarland@gmail.com>
2021-09-09 13:11:16 -0700

    one more change

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ export default function Home() {
     <div className="h-screen w-screen bg-gray-700 flex items-center justify-center">
       <div className="text-white">
         <h1 className="text-5xl t">Hi, Panago Team! 👋</h1>
-        <p>Tom, loves, Ruby!</p>
+        <p>
+          Tom, loves, Ruby! <small>jk</small>
+        </p>
       </div>
     </div>
   );
