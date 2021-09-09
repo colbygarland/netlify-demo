8e4b323ed31fdb927286423bc0aa43284f430ec5
Colby Garland <colbygarland@gmail.com>
2021-09-09 13:10:12 -0700

    Updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-gray-700 flex items-center justify-center">
-      <h1 className="text-5xl text-white">Hi, Panago Team! 👋</h1>
+      <div className="text-white">
+        <h1 className="text-5xl t">Hi, Panago Team! 👋</h1>
+        <p>Tom, loves, Ruby!</p>
+      </div>
     </div>
   );
 }
